135ac71c831d4381e2952946e9b82856d38d00e7
RawSalmon <phanthawasjira@Gmail.com>
2024-02-21 00:38:19 +0700

    Added REST api descriptions

--- Assignment 4/controllers/hospitals.js
+++ Assignment 4/controllers/hospitals.js
@@ -1,19 +1,34 @@
+//@desc     Get all hospitals
+//@route    GET /api/v1/hospitals
+//@access   Public
 exports.getHospitals = (req,res,next) => {
     res.status(200).json({success : true, msg:'Show all hospitals'});
 };
 
+//@desc     Get single hospital
+//@route    GET /api/v1/hospitals/:id
+//@access   Public
 exports.getHospital = (req,res,next) => {
     res.status(200).json({success : true, msg:`Show hospital ${req.params.id}`});
 };
 
+//@desc     Create single hospital
+//@route    POST /api/v1/hospitals
+//@access   Private
 exports.createHospital = (req,res,next) => {
     res.status(200).json({success : true, msg:'Create new hospitals'});
 };
 
+//@desc     Update single hospital
+//@route    PUT /api/v1/hospitals/:id
+//@access   Private
 exports.updateHospital = (req,res,next) => {
     res.status(200).json({success : true, msg:`Update hospital ${req.params.id}`});
 };
 
+//@desc     Delete single hospital
+//@route    DELETE /api/v1/hospitals/:id
+//@access   Private
 exports.deleteHospital = (req,res,next) => {
     res.status(200).json({success : true, msg:`Delete hospital ${req.params.id}`});
 };
